0dbce89058feb4ef21b4d968e6ee6c9ff39a5010
jy-hwang <stratojy@gmail.com>
2022-11-07 12:11:13 +0900

    첫번째커밋

---
+++ src/git/HelloGit.java
@@ -0,0 +1,13 @@
+package git;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Git  :)");
+        System.out.println("이클립스 - git 연동해보기");
+        
+    }
+    
+    
+}
